f00af942c1f1d6f00d2274e2f5c2b30c532229cd
JohnMcLean-dev <john1.mclean@outlook.com>
2022-11-12 17:39:45 -0500

    obtain coordinates from map on click event

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
+import { MapContainer, TileLayer, useMapEvents, Marker, Popup } from 'react-leaflet'
+
+// const position: [number, number] = [1, 1];
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null)
+  const map = useMapEvents({
+    click(e) {
+      const lat: number = e.latlng.lat;
+      const lng: number = e.latlng.lng;
+      console.log(lat, lng)
+    }
+  })
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer center={[0, 0]} zoom={2} scrollWheelZoom={true}>
+      {/* basemap here */}
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {/* <Marker position={[51.505, -0.09]}> */}
+      <LocationMarker />
+    </MapContainer >
   );
 }
 
